8bfbcca19db77a5eee4cfb9f330254b60c3a2b08
Miketsu <miketsudev@gmail.com>
2024-03-29 21:40:26 +0000

    garbage

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,26 @@ function App() {
 
   return (
     <>
-     
+      <TitlePage></TitlePage>
     </>
   );
 }
-
+function TitlePage(){
+  return(
+    <div className="titlePage">
+      <div id="left">Left</div>
+      <div id="center">Center</div>
+      <div id="rigth">Rigth</div>
+    </div>
+    
+  );
+}
+function Title(){
+  return(
+    <h1 className="title" >
+      PixelArt TOOL
+    </h1>
+  );
+  
+}
 export default App;
